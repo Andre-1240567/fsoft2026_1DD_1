2d31d31fbb2096f6dc650ab307d6a8d0fdbaf4e6
Andre-1240567 <1240567@isep.ipp.pt>
2026-05-12 20:16:29 +0100

    SSDs adicionados para as UCs

--- src/view/Utils.cpp
+++ src/view/Utils.cpp
@@ -5,27 +5,21 @@
 #include "Utils.h"
 #include <iostream>
 #include <limits>
-#include <conio.h> // Biblioteca para Windows
 
 void clearScreen() {
-
-    #ifdef _WIN32 //é uma diretiva do compilador. Usa o comando cls (do CMD do Windows). Se estiver em Linux ou Mac, usa o comando clear (do terminal Unix).
-        system("cls");
-    #else
-        system("clear");
-    #endif
-
-    //std::cout << std::string(50, '\n');
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
 }
+
 int readInt(const std::string& prompt, int min, int max) {
     int value;
     while (true) {
-        std::cout << prompt; // (ex: "Opcao: ")
+        std::cout << prompt;
         if (std::cin >> value && value >= min && value <= max) {
-            //std::cin >> value: Tenta puxar algo para value
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            // limpa o Buffer
-            //Ignora o número máximo de caracteres possível (numeric_limits::max()) até encontrares o Enter (\n)". Ou seja, limpa o lixo todo.
             return value;
         }
         std::cin.clear();
@@ -36,6 +30,6 @@ int readInt(const std::string& prompt, int min, int max) {
 }
 
 void pause() {
-    std::cout << "\nPressione qualquer tecla para continuar...";
-    _getch();
+    std::cout << "\nPressione ENTER para continuar...";
+    std::cin.get();
 }
