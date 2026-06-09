37f37ce968c766c49d5cd31e0b33e4f17c8eaeeb
Goncalo-S-Oliveira <goncalodsilvaoliveira@gmail.com>
2026-06-09 09:29:17 +0100

    Empletação do header do SNSUser
     Empletação do header do SNSUser para as UCs do receptionist

--- src/model/SNSUser.cpp
+++ src/model/SNSUser.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "SNSUser.h"
+
+SNSUser::SNSUser(std::string snsNumber, std::string name, std::string address,
+                 std::string birthdate, std::string phone,
+                 std::string citizenCardNumber, std::string sex)
+{
+    this->snsNumber         = snsNumber;
+    this->name              = name;
+    this->address           = address;
+    this->birthdate         = birthdate;
+    this->phone             = phone;
+    this->citizenCardNumber = citizenCardNumber;
+    this->sex               = sex;
+}
+std::string SNSUser::getSnsNumber()         const { return snsNumber; }
+std::string SNSUser::getName()              const { return name; }
+std::string SNSUser::getAddress()           const { return address; }
+std::string SNSUser::getBirthdate()         const { return birthdate; }
+std::string SNSUser::getPhone()             const { return phone; }
+std::string SNSUser::getCitizenCardNumber() const { return citizenCardNumber; }
+std::string SNSUser::getSex()               const { return sex; }
